14b280b7fbd8fec04f1669805556116f88c8cc2c
Anthony <anthonymarques@outlook.com>
2019-08-28 20:11:53 +0200

    i'm working on it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ngxxx-table';
+export class AppComponent implements OnInit{
+  rows = [];
+  loadingIndicator: boolean = true;
+  reorderable: boolean = true;
+
+  columns = [
+    { prop: 'name' },
+    { name: 'Gender' }
+  ];
+
+  ngOnInit() {
+
+  }
+
+  
 }
---
+++ src/app/service/mockdata.service.ts
@@ -0,0 +1,18 @@
+import { CurrencyResponseModel } from "../models/currency-response.model";
+
+export const mockData: [] = [
+  {
+    currencyName: "Netherlands Antillean Guilder",
+    currencyCode: "ANG",
+    absaTraded: "N",
+    majorIndicator: "N",
+    permissibleDailyVolatility: 0.000002
+  },
+  {
+    currencyName: "Nigerian Autonomous Foreign Exchange Rate",
+    currencyCode: "NGX",
+    absaTraded: "Y",
+    majorIndicator: "Y",
+    permissibleDailyVolatility: 10.000000122
+  }
+];
